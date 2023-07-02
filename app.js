b3dc46c6996b50c793d79123d28bbd36ea9f7be0
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-02 18:17:11 +0900

    404 에러 오류 처리, new campground validation 오류 처리

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Campground = require("./models/campground");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
 
 // mongoose and mongoDB connection
 mongoose
@@ -38,6 +39,8 @@ app.get(
 app.post(
   "/campgrounds",
   wrapAsync(async (req, res, next) => {
+    if ((!req.body, campground))
+      throw new ExpressError("Invalid Campground Data", 400);
     const campground = new Campground(req.body.campground);
 
     await campground.save();
@@ -105,8 +108,13 @@ app.delete(
   })
 );
 
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send("Oh boy, Something went Worng!");
+  const { message, statusCode = 500 } = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(PORT, () => console.log(`Server Listening on PORT : ${PORT}`));
