7c0986f45e62ca7a9db56c434f50d60a74bc90bc
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-02 19:05:46 +0900

    Joi 스키마 유효성 검사

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
 
 // mongoose and mongoDB connection
 mongoose
@@ -39,8 +40,28 @@ app.get(
 app.post(
   "/campgrounds",
   wrapAsync(async (req, res, next) => {
-    if ((!req.body, campground))
-      throw new ExpressError("Invalid Campground Data", 400);
+    // if ((!req.body, campground))
+    //   throw new ExpressError("Invalid Campground Data", 400);
+
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+      }).required(),
+    });
+
+    const result = campgroundSchema.validate(req.body);
+
+    const { error } = result;
+
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(msg, 400);
+    }
+
     const campground = new Campground(req.body.campground);
 
     await campground.save();
