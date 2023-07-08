d09084946cabaddc4010313b09d1e87531208c5d
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-08 21:08:44 +0900

    해당 캠핑장의 리뷰 스타일링 해보기

--- schema.js
+++ schema.js
@@ -12,7 +12,7 @@ module.exports.campgroundSchema = Joi.object({
 
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
-    body: Joi.string().required().min(1).max(5),
-    rating: Joi.number().required(),
+    body: Joi.string().required(),
+    rating: Joi.number().required().min(1).max(5),
   }).required(),
 });
