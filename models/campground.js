67dce944f6466097765aa8c663249787c9a02a31
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-08 21:50:40 +0900

    캠프그라운드 미들웨어 삭제

--- models/campground.js
+++ models/campground.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
-const campgroundSchema = new Schema({
+const campgrouncdSchema = new Schema({
   title: String,
   image: String,
   price: Number,
@@ -10,6 +11,16 @@ const campgroundSchema = new Schema({
   reviews: { type: [Schema.Types.ObjectId], ref: "Review" },
 });
 
-const Campground = mongoose.model("Campground", campgroundSchema);
+campgrouncdSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
+
+const Campground = mongoose.model("Campground", campgrouncdSchema);
 
 module.exports = Campground;
