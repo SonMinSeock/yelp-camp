e38cebd1c0c25e2f0b3e0d9db6d37ac7ed1b45dc
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-08 21:28:36 +0900

    해당 캠핑장의 리뷰 지워보기

--- models/review.js
+++ models/review.js
@@ -6,4 +6,5 @@ const reviewSchema = new Schema({
   rating: Number,
 });
 
-module.exports = mongoose.model("Review", reviewSchema);
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
