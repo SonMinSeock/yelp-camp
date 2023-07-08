f9dd517120c5e6340b9ca6ae8b40b41c3e3821b0
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-08 19:45:22 +0900

    리뷰 모델 정의하기, 관계형 모댈 정의 해주기

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const campgroundSchema = new mongoose.Schema({
+const campgroundSchema = new Schema({
   title: String,
   image: String,
   price: Number,
   description: String,
   location: String,
+  reviews: { type: [Schema.Types.ObjectId], ref: "Review" },
 });
 
 const Campground = mongoose.model("Campground", campgroundSchema);
