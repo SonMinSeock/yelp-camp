4bed037d5f8134e63eab921e5cf6c042417ac304
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-08-06 19:25:41 +0900

    회원가입 라우터 에러처리 적용

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-  const {
-    body: { username, email, password },
-  } = req;
+router.post(
+  "/register",
+  wrapAsync(async (req, res) => {
+    try {
+      const {
+        body: { username, email, password },
+      } = req;
 
-  res.send(req.body);
-});
+      const user = new User({
+        username,
+        email,
+      });
+
+      const registerUser = await User.register(user, password);
+
+      console.log(registerUser);
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/register");
+    }
+  })
+);
 
 module.exports = router;
