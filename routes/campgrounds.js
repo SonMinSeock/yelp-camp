fd6854cf8a4105c8472dc722941fdced2c1259ae
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-08-26 17:17:54 +0900

    enctype 알아보기, Multer 미들웨어 사용

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,11 +4,16 @@ const wrapAsync = require("../utils/wrapAsync");
 const Campground = require("../models/campground");
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
 const campgrounds = require("../controllers/campgrounds");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router
   .route("/")
   .get(wrapAsync(campgrounds.index))
-  .post(isLoggedIn, validateCampground, wrapAsync(campgrounds.createCampground));
+  // .post(isLoggedIn, validateCampground, wrapAsync(campgrounds.createCampground));
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+  });
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
