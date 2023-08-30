8b66dd54cf67171b5a20021de5dea40c294e2753
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-08-30 17:46:06 +0900

    Cloudinary 이미지 업로드 해보기

---
+++ cloudinary/index.js
@@ -0,0 +1,19 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINRY_CLOUS_NAME,
+  api_key: process.env.CLOUDINRY_KEY,
+  api_secret: process.env.CLOUDINRY_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  folder: "YelpCamp",
+  allowedFormats: ["jpeg", "png", "jpg"],
+});
+
+module.exports = {
+  cloudinary,
+  storage,
+};
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,7 +5,8 @@ const Campground = require("../models/campground");
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
 const campgrounds = require("../controllers/campgrounds");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudinary/index");
+const upload = multer({ storage });
 
 router
   .route("/")
@@ -13,6 +14,7 @@ router
   // .post(isLoggedIn, validateCampground, wrapAsync(campgrounds.createCampground));
   .post(upload.array("image"), (req, res) => {
     console.log(req.body, req.files);
+    res.send("It worked!");
   });
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
